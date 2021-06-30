e8a9f11827f7ed6b5af47cadf3cf6656e497afc3
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-30 15:01:53 +0200

    Add fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'title', 'body', 'image'];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
