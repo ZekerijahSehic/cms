ddd29f73064b61d287467b6fb689e7ea016fba62
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-06 19:03:35 +0200

    CRUD methodes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,20 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
 
-    public function allPosts()
+    public function index()
     {
-        return view('landing', [
-            'posts' => Post::latest()->paginate(5)
-        ]);
+        return PostResource::collection(Post::latest()->paginate(5));
     }
 
-    public function showPost(Post $post)
+    public function show(Post $post)
     {
-        return view('single', compact('post'));
+        return PostResource($post);
     }
 
     public function create()
@@ -27,8 +26,32 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'user_id' => 'required',
+            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $post = new Post;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = str_slug($request->title).'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/uploads/posts');
+            $imagePath = $destinationPath . "/" . $name;
+            $image->move($destinationPath, $name);
+            $post->image = $name;
+        }
+
+        $post->user_id = $request->user_id;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return new PostResource($post);
     }
+
 
     public function edit(Post $post)
     {
@@ -37,10 +60,20 @@ class PostController extends Controller
 
     public function update(Request $request, Post $post)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post->update($request->only(['title', 'body']));
+
+        return new PostResource($post);
     }
+
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response()->json(null, 204);
     }
 }
